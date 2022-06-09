b1a4f79f94e8f26999ccd5bcb2cac1066816ad7b
kliastro13 <it.goncharov@gmail.com>
2022-06-09 23:35:13 +0300

    add "Print" method

--- js/Modules/module.js
+++ js/Modules/module.js
@@ -1,7 +1,8 @@
 class ListElement {
-  constructor(lastName, next = null) {
+  constructor(lastName, next = null, index) {
     this.lastName = lastName;
     this.next = next;
+    this.index = index;
   }
 }
 
@@ -16,7 +17,29 @@ class EmployeesList {
 
   Delete(index) {}
 
-  Print(index) {}
+  Print(index) {
+    //for for empty list
+    if (!this.start) {
+      return console.log(`Employees list is empty`);
+    }
+    //for whole list
+    if (index === undefined) {
+      let element = this.start;
+      while (element) {
+        console.log(element.lastName);
+        element = element.next;
+      }
+    }
+    //for any element
+    let element = this.start;
+    while (element !== null) {
+      if (element.index === index) return console.log(element.lastName);
+      element = element.next;
+    }
+    return console.log(
+      `There is no element with index ${index} in Employees list`
+    );
+  }
 
   Count() {
     console.log(`Employees list has ${this.size} elements`);
@@ -24,23 +47,26 @@ class EmployeesList {
 
   Add(lastName) {
     const element = new ListElement(lastName);
+    //for empty list
     if (this.size === 0) {
       this.start = element;
+      element.index = this.size;
       this.size++;
       return;
     }
-
+    //for list with only 1 element
     if (this.size === 1) {
       this.start.next = element;
       this.end = element;
+      element.index = this.size;
       this.size++;
       return;
     }
-
+    //for all other cases
     if (this.end) {
       this.end.next = element;
+      element.index = this.size;
     }
-
     this.size++;
     this.end = element;
   }
