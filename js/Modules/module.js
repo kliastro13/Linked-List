248858ede5cceb9df2c66b3d1b7081a441c00316
kliastro13 <it.goncharov@gmail.com>
2022-06-09 20:23:15 +0300

    added "Add" method, added test object in main js

--- js/Modules/module.js
+++ js/Modules/module.js
@@ -1,6 +1,6 @@
 class ListElement {
-  constructor(data, next = null) {
-    this.data = data;
+  constructor(lastName, next = null) {
+    this.lastName = lastName;
     this.next = next;
   }
 }
@@ -10,5 +10,36 @@ class EmployeesList {
     this.start = null;
     this.end = null;
     this.size = 0;
+  }
+
+  Insert(index, lastName) {}
+
+  Delete(index) {}
+
+  Print(index) {}
+
+  Count() {}
+
+  Add(lastName) {
+    const element = new ListElement(lastName);
+    if (this.size === 0) {
+      this.start = element;
+      this.size++;
+      return;
+    }
+
+    if (this.size === 1) {
+      this.start.next = element;
+      this.end = element;
+      this.size++;
+      return;
+    }
+
+    if (this.end) {
+      this.end.next = element;
+    }
+
+    this.size++;
+    this.end = element;
   }
 }
---
+++ js/main.js
@@ -0,0 +1,10 @@
+const list1 = new EmployeesList();
+
+list1.Add("Kolisnyk");
+list1.Add("Kurmach");
+list1.Add("Sokolovskiy");
+list1.Add("Kryvoruchka");
+list1.Add("Denysov");
+list1.Add("Kudimov");
+
+console.log(list1);
